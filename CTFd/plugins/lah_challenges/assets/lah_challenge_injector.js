98bd1775093448b3e855374cc08324fa70f30a99
Josh Hejna <josh.hejna@gmail.com>
2019-03-23 22:09:33 -0700

    Make selected challenge blue

--- CTFd/plugins/lah_challenges/assets/lah_challenge_injector.js
+++ CTFd/plugins/lah_challenges/assets/lah_challenge_injector.js
@@ -16,7 +16,7 @@ if (typeof loadchals !== 'undefined') {
                         $(this).css("border", "none")
                     }
                     if (info.selected === id) {
-                        $(this).css("background-color", "#d66436");
+                        $(this).css("background-color", "#5cbeff");
                         $(this).css("opacity", "0.7");
                         $(this).css("border", "none")
                     }
